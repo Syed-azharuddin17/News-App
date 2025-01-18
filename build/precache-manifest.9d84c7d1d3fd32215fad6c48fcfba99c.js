d17769e1b11955066276091fc10dda145395ab62
Syed-azharuddin17 <syedazharuddin0081@gmail.com>
2025-01-18 14:39:03 -0800

    added api key in .env file

---
+++ build/precache-manifest.9d84c7d1d3fd32215fad6c48fcfba99c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c529690e6f61a11e12bf",
+    "url": "/static/css/main.cbdafe1e.chunk.css"
+  },
+  {
+    "revision": "c529690e6f61a11e12bf",
+    "url": "/static/js/main.c529690e.chunk.js"
+  },
+  {
+    "revision": "046e05133628af1d8cc8",
+    "url": "/static/js/1.046e0513.chunk.js"
+  },
+  {
+    "revision": "9da398fd6ac17a98338c",
+    "url": "/static/js/2.9da398fd.chunk.js"
+  },
+  {
+    "revision": "4c03d365511855e42ece",
+    "url": "/static/js/runtime~main.4c03d365.js"
+  },
+  {
+    "revision": "a90e737d05ebfa82bf96168def807c36",
+    "url": "/static/media/loading.a90e737d.gif"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1c26d25c5e0d3333750fd99344437596",
+    "url": "/index.html"
+  }
+];
